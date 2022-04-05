8ec8925db26be5c3e11ba9e1a05bed0ca4df0cfd
Alex-A-B <alexjamesbelton@gmail.com>
2022-04-05 11:00:12 +0100

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     
+    validates :username, uniqueness: true
+    validates :username, :password, presence: true on: :create
+
+    has_many :orders
 end
